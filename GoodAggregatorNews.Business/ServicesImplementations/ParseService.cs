305a981aadc143c387d3cb10a36b425d8760fdc3
KatsiarynaKholad <100599536+KatsiarynaKholad@users.noreply.github.com>
2022-11-20 21:03:18 +0300

    parse

--- GoodAggregatorNews.Business/ServicesImplementations/ParseService.cs
+++ GoodAggregatorNews.Business/ServicesImplementations/ParseService.cs
@@ -44,8 +44,11 @@ namespace GoodAggregatorNews.Business.ServicesImplementations
                            || node.Name.Equals("h1")
                            || node.Name.Equals("p"))
                                        && !node.HasClass("article__reference")
+                                       && !node.HasClass("global-incut")
+                                       && !node.HasClass("card")
+                                       && !node.HasClass("custom-block")
                                        && !node.HasClass("article-meta")
-                                       && node.Attributes["style"] == null)
+                           && node.Attributes["span class='article - meta'"] == null)
                            .Select(node => node.InnerText.Trim())
                            .Aggregate((i, j) => i + Environment.NewLine + j);
 
@@ -53,51 +56,6 @@ namespace GoodAggregatorNews.Business.ServicesImplementations
                         await _unitOfWork.Commit();
 
                     }
-
-
-                    //var nodes =
-                    //    htmldoc.DocumentNode.Descendants(0)
-                    //    .Where(n => n.HasClass("article__container"));
-
-                    //if (nodes.Any())
-                    //{
-                    //    var articleText = nodes.FirstOrDefault()?
-                    //       .ChildNodes
-                    //       .Where(node => (node.Name.Equals("p"))
-                    //                   && !node.HasClass("incut")
-                    //                   && !node.HasClass("article__lead")
-                    //                   && !node.HasClass("global-incut")
-                    //                   && node.Attributes["style"] == null)
-                    //       .Select(node => node.InnerText)
-                    //       .Aggregate((i, j) => i + Environment.NewLine + j);
-
-                    //    await _unitOfWork.Articles.UpdateArticleTextAsync(articleid, articleText);
-                    //    await _unitOfWork.Commit();
-
-                    //}
-
-                    //if (nodes.Any())
-                    //{
-                    //    var articleText = nodes.FirstOrDefault()?
-                    //       .ChildNodes
-                    //       .Where(node => (node.Name.Equals("h1")
-                    //       || node.Name.Equals("p")
-                    //       || node.Name.Equals("div"))
-                    //                    && !node.HasClass("article__reference article__reference_header")
-                    //                    && !node.HasClass("article-meta__item")
-                    //                    && !node.HasClass("share")
-                    //                    && !node.HasClass("global-incut")
-                    //                    && !node.HasClass("custom-block rotation-1")
-                    //                    && !node.HasClass("custom-block")
-                    //                    && !node.HasClass("article__footer")
-                    //                    && node.Attributes["style"] == null)
-                    //       .Select(node => node.InnerText)
-                    //       .Aggregate((i, j) => i + Environment.NewLine + j);
-
-                    //    await _unitOfWork.Articles.UpdateArticleTextAsync(articleid, articleText);
-                    //    await _unitOfWork.Commit();
-
-                    //}
 
                 }
 
